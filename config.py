6cdf4456e445fa95447400f9d64d29a1a499e4ad
Pathan Arshlan <141414515+Arshlankhan786@users.noreply.github.com>
2024-01-14 13:03:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://pathan-public-store-7035eaa9d8a8.herokuapp.com/"
+        URL = "https://pathanbotz-26bec0548e24.herokuapp.com/"
     else:
-        URL = "https://pathan-public-store-7035eaa9d8a8.herokuapp.com/"
+        URL = "https://pathanbotz-26bec0548e24.herokuapp.com/"
 
 
 
