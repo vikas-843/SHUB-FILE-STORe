3451dc4507efb42d221e8625d13b5af20c3a4e8f
Pathan Arshlan <141414515+Arshlankhan786@users.noreply.github.com>
2024-01-18 17:19:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ DB_NAME = environ.get("DB_NAME", "Pathanpublicbotz")
 
 # Bot Information
 BOT_TOKEN = environ.get("BOT_TOKEN", "6473247597:AAHtGqrI9bFbAFETcAPMNF45D2iWYKjrSEM")
-BOT_USERNAME = environ.get("BOT_USERNAME", "Spike_Spiegel_files_bot) # your bot username without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "Spike_Spiegel_files_bot") # your bot username without @
 PICS = (environ.get('PICS', 'https://graph.org/file/82ef767ffebe3a948e476.jpg https://graph.org/file/82ef767ffebe3a948e476.jpg')).split() # Bot Start Picture
 
 # Auto Delete Information
