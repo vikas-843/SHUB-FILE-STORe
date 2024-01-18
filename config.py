71766c7fe211d5b5f64143f9c8436ae4c8a1d70a
Pathan Arshlan <141414515+Arshlankhan786@users.noreply.github.com>
2024-01-18 16:48:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,8 +45,8 @@ DB_NAME = environ.get("DB_NAME", "Pathanpublicbotz")
 # Ask Doubt on telegram @KingVJ01
 
 # Bot Information
-BOT_TOKEN = environ.get("BOT_TOKEN", "6949137221:AAGc6FUnobcZe03vaMEK1gLLrlsy7KxL76Y")
-BOT_USERNAME = environ.get("BOT_USERNAME", "Public_store_pathanbot") # your bot username without @
+BOT_TOKEN = environ.get("BOT_TOKEN", "6473247597:AAHtGqrI9bFbAFETcAPMNF45D2iWYKjrSEM")
+BOT_USERNAME = environ.get("BOT_USERNAME", "Spike_Spiegel_files_bot) # your bot username without @
 PICS = (environ.get('PICS', 'https://graph.org/file/82ef767ffebe3a948e476.jpg https://graph.org/file/82ef767ffebe3a948e476.jpg')).split() # Bot Start Picture
 
 # Auto Delete Information
@@ -54,8 +54,8 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "7200")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "432000")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001915187457"))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1001915187457')).split()]
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002065194475"))
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002065194475')).split()]
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
@@ -79,7 +79,7 @@ class Var(object):
     name = str(getenv('name', 'filetolinkpathanbotz'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001915187457'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002065194475'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://pathanbotz-26bec0548e24.herokuapp.com/"
+        URL = ""
     else:
-        URL = "https://pathanbotz-26bec0548e24.herokuapp.com/"
+        URL = ""
 
 
 
