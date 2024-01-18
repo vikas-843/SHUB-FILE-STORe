4d7e47f3a922a2eefdd5be424cf66dae5c8be2fb
Pathan Arshlan <141414515+Arshlankhan786@users.noreply.github.com>
2024-01-18 17:29:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -97,7 +97,7 @@ async def start():
     await app.setup()
     bind_address = "0.0.0.0"
     await web.TCPSite(app, bind_address, Var.PORT).start()
-    await restart_bots()
+    
     print("Bot Started Powered By @pathan_botz")
     await idle()
 
